320d5a109e3362798c25132ba93afa732cb27e8c
OverRevvv <rdude1612@gmail.com>
2023-06-09 19:56:48 +0530

    Removed lazy loader

--- Client/src/router.js
+++ Client/src/router.js
@@ -1,20 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router';
-// import Home from './components/Home.vue';
-// import Anime from ('./components/Anime.vue')
-// import Cars from('./components/Cars.vue')
-// import  Tech from('./components/Tech.vue')
-
-//* Lazy Loading
+import Home from './components/Home.vue';
+import Anime from './components/Anime.vue'
+import Cars from './components/Cars.vue'
+import Tech from './components/Tech.vue'
 
 const routes = [
-  { path: '/', component: import('./components/Home.vue')},
-  { path: '/anime', component: import('./components/Anime.vue')},
-  { path: '/cars', component: import('./components/Cars.vue')},
-  { path: '/tech', component: import('./components/Tech.vue')},
+  { path: '/', component: Home },
+  { path: '/anime', component: Anime },
+  { path: '/cars', component: Cars },
+  { path: '/tech', component: Tech },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
 export default router;
