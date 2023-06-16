9613a0d29b30d08644000e910244f6ade7dd4a60
OverRevvv <rdude1612@gmail.com>
2023-06-16 12:59:41 +0530

    udpated vite lazy loader and build

--- Client/src/router.js
+++ Client/src/router.js
@@ -3,12 +3,15 @@ import Home from './components/Home.vue';
 // import Anime from './components/Anime.vue'
 // import Cars from './components/Cars.vue'
 // import Tech from './components/Tech.vue'
+const Anime = () => import('./components/Anime.vue');
+const Cars = () => import('./components/Cars.vue');
+const Tech = () => import('./components/Tech.vue');
 
 const routes = [
   { path: '/', component: Home },
-  { path: '/anime', component: import('./components/Anime.vue') },
-  { path: '/cars', component: import('./components/Cars.vue') },
-  { path: '/tech', component: import('./components/Tech.vue') },
+  { path: '/anime', component: Anime },
+  { path: '/cars', component: Cars },
+  { path: '/tech', component: Tech },
 ];
 
 const router = createRouter({
